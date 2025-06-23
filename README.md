7058c8c09dcff25e8b21abf06a1c0e060b33ddd5
Garrett Simpson <garrett@dotunwrap.dev>
2025-06-23 19:37:59 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# .unwrap's nvf config
+
+This is a WIP [nvf](https://github.com/notashelf/nvf) configuration.
+
+nvf is a framework for structuring your Neovim configuration using Nix.
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nvf.url = "github:notashelf/nvf";
+  };
+
+  outputs =
+    { flake-parts
+    , nvf
+    , ...
+    } @ inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { pkgs, ... }:
+        let
+          configModule = import ./nvf-config.nix;
+          nvimConfig = nvf.lib.neovimConfiguration {
+            modules = [ configModule ];
+            inherit pkgs;
+          };
+        in
+        {
+          packages.default = nvimConfig.neovim;
+          formatter = pkgs.alejandra;
+        };
+    };
+}
